6e6ccf1ad45ed2a582f174e992d3b3ffd148a033
ewiener55-dotcom <ewiener55@gmail.com>
2026-01-13 16:22:23 -0500

    Delete chat directory

--- chat/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'chat'
-
-urlpatterns = [
-    path('', views.index_view, name='index'),  # Landing page
-    path('chat/', views.chat_view, name='chat'),  # API endpoint
-]
